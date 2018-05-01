2e78c74e6ec960bcf956dcd8d16183d352f30ecf
Andi Prianto <andi.prianto@gmail.com>
2018-05-01 08:09:33 +0700

    update task

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  // return gulp.src('./src/**/[^_]*.scss')
   return gulp.src(['./scss/**/*.scss', '!./scss/**/_*.scss', '!./scss/**/_tmp/**/*'])
     .pipe(sass())
     .pipe(gulp.dest(function(file) {
       var base = file.base;
-      return base.replace('scss/', 'src/');
+      console.log(base);
+      return base.replace('scss', 'src');
     }));
 });
 
